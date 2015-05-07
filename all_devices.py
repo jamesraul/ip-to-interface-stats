f4d161e668bbf9584a980a7aadb8facb313304e8
James Raulinaitis <jamesraul@gmail.com>
2015-05-07 14:18:35 -0400

    Create all_devices.py

---
+++ all_devices.py
@@ -0,0 +1,34 @@
+ALL_DEVICES = {
+          'R1': {
+          'ip': '10.0.0.2',
+          'device_type': 'cisco_ios',
+          'username': 'admin',
+          'password': 'cisco',
+          'secret': 'cisco',
+          'verbose': False
+          },
+          'R2': {
+          'ip': '10.0.0.3',
+          'device_type': 'cisco_ios',
+          'username': 'admin',
+          'password': 'cisco',
+          'secret': 'cisco',
+          'verbose': False
+          },
+          'ESW1': {
+          'ip': '10.0.0.11',
+          'device_type': 'cisco_ios',
+          'username': 'admin',
+          'password': 'cisco',
+          'secret': 'cisco',
+          'verbose': False
+          },
+          'ESW2': {
+          'ip': '10.0.0.12',
+          'device_type': 'cisco_ios',
+          'username': 'admin',
+          'password': 'cisco',
+          'secret': 'cisco',
+          'verbose': False
+          }
+}
